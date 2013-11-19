6d4e35f2ae9798e36b6cabdd9ed6d151b20911a1
Michael Williams <dinosaur@riseup.net>
2013-11-19 00:15:34 -0800

    got basics of multiple virtual strips within a single physical strip working

--- beatpixels.ino
+++ beatpixels.ino
@@ -20,16 +20,66 @@ CRGB leds[NUM_LEDS];
 
 OSCBundle bundleOUT;
 
-/* various led dispatches */
+/* strip definition */
+typedef struct {
+  int start;
+  int end;
+} Strip;
 
-void dispatchRGB(OSCMessage &msg)
+Strip strips [2] = {
+  { 0, 15 },
+  { 16, 31 }
+};
+
+/* RGB utilities */
+
+typedef struct {
+  CRGB * vals;
+  int len;
+} CRGBS;
+
+/* set RGB colors */
+void setRGB(int stripNum, CRGBS &colors)
 {
-  int red = msg.getInt(0);
-  int green = msg.getInt(1);
-  int blue = msg.getInt(2);
-  LEDS.showColor(CRGB(red, green, blue));
-  bundleOUT.add("/leds/rgb").add(red).add(green).add(blue);
+  int pxStart = strips[stripNum].start;
+  int pxEnd = strips[stripNum].end;
+  for (int px = pxStart; px < pxEnd; px++)
+  {
+    CRGB color = colors.vals[px % colors.len];
+    leds[px] = colors.vals[px % colors.len];
+  }
 }
+
+void getRGB(OSCMessage &msg, CRGBS &colors)
+{
+  /* get rgb colors */
+  int msgSize = msg.size();
+  if (msgSize && ((msgSize % 3) != 0))
+  {
+    bundleOUT.add("/error/num/rgb/colors");
+    return;
+  }
+  int numColors = msgSize / 3;
+  bundleOUT.add("/numColors").add(numColors);
+  CRGB * vals = (CRGB *) malloc(numColors * sizeof(CRGB));
+  for (int i = 0; i < numColors; i++)
+  {
+    int red = msg.getInt(i * 3);
+    int green = msg.getInt(i * 3 + 1);
+    int blue = msg.getInt(i * 3 + 2);
+    bundleOUT.add("/rgb").add(red).add(green).add(blue);
+    vals[i] = CRGB(red, green, blue);
+  }
+  colors.len = numColors;
+  colors.vals = vals;
+}
+
+void setHSV(int stripNum, CHSV color[])
+{
+  
+}
+
+/* various led dispatches */
 
 void dispatchRainbow(OSCMessage &msg)
 {
@@ -38,37 +88,76 @@ void dispatchRainbow(OSCMessage &msg)
   fill_rainbow(leds, NUM_LEDS, startingHue);
   bundleOUT.add("/leds/rainbow");
 }
-/*
+      
+/* main controller for led routes 
+
 /leds
   /strip/[0-9]
+    /one/[0-9]
+    /many/[0-9]/[0-9]/.../endmany
     /from/[0-9]/to/[0-9]
     /all
       /rgb
       /rainbow
       /hsv
         color1, color2, ...
-      
-/* main controller for led routes */
+
+*/
 void routeLeds(OSCMessage &msg, int ledsOffset)
 {
-  bundleOUT.add("/routeLeds");
-
-  if (msg.fullMatch("/rgb", ledsOffset)) {
-    msg.dispatch("/rgb", dispatchRGB, ledsOffset);
-  } else if (msg.fullMatch("/rainbow", ledsOffset)) {
-    msg.dispatch("/rainbow", dispatchRainbow, ledsOffset);
-  } else if (msg.fullMatch("/red", ledsOffset)) {
-    LEDS.showColor(CRGB::Red);
-    bundleOUT.add("/leds/red");
-  } else if (msg.fullMatch("/green", ledsOffset)) {
-    LEDS.showColor(CRGB::Green);
-    bundleOUT.add("/leds/green");
-  } else if (msg.fullMatch("/blue", ledsOffset)) {
-    LEDS.showColor(CRGB::Blue);
-    bundleOUT.add("/leds/blue");
+  char address [30];
+  int addressLen = msg.getAddress(address, ledsOffset);
+  bundleOUT.add("/leds").add(address).add(addressLen);
+  
+  /* get strip number */
+  int startStrip = msg.match("/strip", ledsOffset);
+  int stripOffset = msg.match("/strip/*", ledsOffset);
+  int stripCharsLen =  stripOffset - startStrip;
+  
+  char stripChars [stripCharsLen];
+  strncpy(stripChars, address + startStrip + 1, stripCharsLen);
+  stripChars[stripCharsLen] = '\0';
+  
+  int stripNum = atoi(stripChars);
+  
+  bundleOUT.add("/strip").add(stripNum);
+  
+  /* get whether all, range, or single pixels */
+  
+  int offset = ledsOffset + stripOffset;
+  
+  int allOffset = msg.match("/all", offset);
+  if (allOffset)
+  {
+    offset += allOffset;
+    
+    if (msg.fullMatch("/rgb", offset))
+    { 
+      CRGBS colors;
+      getRGB(msg, colors);
+      setRGB(stripNum, colors);
+      free(colors.vals);
+    } else if (msg.fullMatch("/rainbow", offset)) {
+      msg.dispatch("/rainbow", dispatchRainbow, offset);
+    } else if (msg.fullMatch("/red", offset)) {
+      CRGB vals [] = { CRGB::Red };
+      CRGBS colors = { vals, 1 };
+      setRGB(stripNum, colors);
+      bundleOUT.add("/red");
+    } else if (msg.fullMatch("/green", offset)) {
+      CRGB vals [] = { CRGB::Green };
+      CRGBS colors = { vals, 1 };
+      setRGB(stripNum, colors);
+      bundleOUT.add("/green");
+    } else if (msg.fullMatch("/blue", offset)) {
+      CRGB vals [] = { CRGB::Blue };
+      CRGBS colors = { vals, 1 };
+      setRGB(stripNum, colors);
+      bundleOUT.add("/blue");
+    }
   }
-}
 
+}
 
 void setup() {
   // sanity check delay - allows reprogramming if accidently blowing power w/leds
